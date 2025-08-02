d0b4f16173a21312bf792cc30a2154325a99a9f5
Aaron Zuefeldt <87891525+aaronzuefeldt@users.noreply.github.com>
2025-08-01 22:49:50 -0600

    CPU compatible again

--- tictacshoot/keras/CustomTicTacToeNNet.py
+++ tictacshoot/keras/CustomTicTacToeNNet.py
@@ -1,4 +1,4 @@
-# tictacshoot/keras/CustomTicTacToeNNet.py
+# tictacshoot/keras/CustomTicTacToeNNet.py - CPU-COMPATIBLE VERSION
 
 import sys
 sys.path.append('..')
@@ -11,30 +11,25 @@ from tensorflow.keras.optimizers import *
 
 class CustomTicTacToeNNet():
     def __init__(self, game, args):
-        # Game Parameters
         self.input_shape = game.getInitBoard().shape 
         self.action_size = game.getActionSize()
         self.args = args
 
-        # Neural Net
         self.input_boards = Input(shape=self.input_shape)
 
-        # --- GPU-OPTIMIZED (NCHW / channels-first) VERSION ---
-        # The input is already in (planes, x, y), which is the desired NCHW format.
-        # No Permute layer is needed.
-        x_image = self.input_boards
+        # Reshape input from (planes, x, y) to (x, y, planes) for NHWC format
+        x_image = Permute((2, 3, 1))(self.input_boards)
 
-        # We explicitly tell Keras to use 'channels_first' and set the BatchNormalization axis to 1.
-        h_conv1 = Activation('relu')(BatchNormalization(axis=1)(Conv2D(args.num_channels, 3, padding='same', data_format='channels_first')(x_image)))
-        h_conv2 = Activation('relu')(BatchNormalization(axis=1)(Conv2D(args.num_channels, 3, padding='same', data_format='channels_first')(h_conv1)))
-        h_conv3 = Activation('relu')(BatchNormalization(axis=1)(Conv2D(args.num_channels, 3, padding='same', data_format='channels_first')(h_conv2)))
-        h_conv4 = Activation('relu')(BatchNormalization(axis=1)(Conv2D(args.num_channels, 3, padding='valid', data_format='channels_first')(h_conv3)))
+        # Use 'channels_last' (default) and BatchNormalization axis 3 for CPU
+        h_conv1 = Activation('relu')(BatchNormalization(axis=3)(Conv2D(args.num_channels, 3, padding='same')(x_image)))
+        h_conv2 = Activation('relu')(BatchNormalization(axis=3)(Conv2D(args.num_channels, 3, padding='same')(h_conv1)))
+        h_conv3 = Activation('relu')(BatchNormalization(axis=3)(Conv2D(args.num_channels, 3, padding='same')(h_conv2)))
+        h_conv4 = Activation('relu')(BatchNormalization(axis=3)(Conv2D(args.num_channels, 3, padding='valid')(h_conv3)))
         
         h_conv4_flat = Flatten()(h_conv4)
         s_fc1 = Dropout(args.dropout)(Activation('relu')(BatchNormalization(axis=1)(Dense(1024)(h_conv4_flat))))
         s_fc2 = Dropout(args.dropout)(Activation('relu')(BatchNormalization(axis=1)(Dense(512)(s_fc1))))
         
-        # Output Heads
         self.pi = Dense(self.action_size, activation='softmax', name='pi')(s_fc2)
         self.v = Dense(1, activation='tanh', name='v')(s_fc2)
 
